0c7b1de58bdf7ebe5c4a64d96e3812bdf45fde75
unknown <ganitniduniyanikunj10@gmail.com>
2023-06-14 16:53:57 +0530

    RestFulWebService

---
+++ Phase-3/Session-3/RestFulWebService/src/main/java/com/simplilearn/demo/UserModel.java
@@ -0,0 +1,52 @@
+package com.simplilearn.demo;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UserModel {
+	
+	private int id;
+	private String name;
+	private String email;
+	private String username;
+	private String website;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getWebsite() {
+		return website;
+	}
+	public void setWebsite(String website) {
+		this.website = website;
+	}
+	@Override
+	public String toString() {
+		return "UserModel [id=" + id + ", name=" + name + ", email=" + email + ", username=" + username + ", website="
+				+ website + "]";
+	}
+	
+	
+	
+
+}
